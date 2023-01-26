38fed7bd546455c12f01339f485cc3b1c98b69b4
Alen Rubilar <lclc.alen@gmail.com>
2023-01-25 16:03:44 -0800

    feat: Refactor repo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from modeling_t5 import T5ForConditionalGeneration
+from transformers import T5Tokenizer
 
 model = T5ForConditionalGeneration.from_pretrained("google/t5-v1_1-small")
---
+++ src/modeling_encoder.py
@@ -0,0 +1,18 @@
+from transformers.modeling_utils import PreTrainedModel
+from transformers.configuration_utils import PretrainedConfig
+
+
+class EncoderConfig(PretrainedConfig):
+    pass
+
+
+class Encoder(PreTrainedModel):
+    pass
+
+
+class DecoderConfig(PretrainedConfig):
+    pass
+
+
+class Decoder(PreTrainedModel):
+    pass
